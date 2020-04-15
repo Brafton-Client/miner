d64d35835490c51cd467f5f236c6b39a71da0ebb
allanjames <james.allan@brafton.com>
2020-04-15 17:22:58 -0400

    expanded logic for vendors plugin

--- wp/wp-content/plugins/miner-vendors/miner-vendors.php
+++ wp/wp-content/plugins/miner-vendors/miner-vendors.php
@@ -6,3 +6,11 @@
  * Author: James Allan
  */
 include 'util/functions.php';
+
+add_shortcode( 'render_vendors', 'vendor_shortcode' );
+  
+function vendor_shortcode() {
+    ob_start();
+    include('templates/vendor-template.php');
+    return ob_get_clean();
+}
--- wp/wp-content/plugins/miner-vendors/util/functions.php
+++ wp/wp-content/plugins/miner-vendors/util/functions.php
@@ -21,7 +21,7 @@ function vendor_setup_post_type() {
         'query_var'          => true,
         'capability_type'    => 'post',
         'has_archive'        => true,
-        'hierarchical'       => false,
+        'hierarchical'       => true,
         'menu_position'      => null,
         'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt', 'taxonomies' ),
     );
@@ -36,40 +36,25 @@ function register_vendor_taxonomies() {
         'singular_name'         => _x( 'Vendor City', 'taxonomy singular name', 'textdomain' )
     );
     register_taxonomy('vendor_city', 'vendor', array(
-        'hierarchical' => false,
+        'hierarchical' => true,
         'labels' => $labels,
         'show_ui' => true,
         'show_admin_column' => true,
         'query_var' => true,
+        //'meta_box_cb' => 'dropdown',
     ));
     $labels = array(
         'name'                  => _x( 'Vendor Products', 'taxonomy general name', 'textdomain' ),
         'singular_name'         => _x( 'Vendor Product', 'taxonomy singular name', 'textdomain' )
     );
     register_taxonomy('vendor_product', 'vendor', array(
-        'hierarchical' => false,
+        'hierarchical' => true,
         'labels' => $labels,
         'show_ui' => true,
         'show_admin_column' => true,
         'query_var' => true,
+        //'meta_box_cb' => 'dropdown',
     ));
 
 }
 add_action( 'init', 'register_vendor_taxonomies' );
-
-/* MakeTemplates Available from this plugin*/
-
-function vendors_add_page_template ($templates) {
-    $templates['vendors-template.php'] = 'Vendors Template';
-    return $templates;
-}
-add_filter ('theme_page_templates', 'vendors_add_page_template');
-
-function vendors_redirect_page_template ($template) {
-    $post = get_post(); 
-    $page_template = get_post_meta( $post->ID, '_wp_page_template', true );
-    if ('vendor-template.php' == basename ($page_template )) 
-        $template = WP_PLUGIN_DIR . '/plugins/miner-vendors/templates/vendor-template.php';
-    return $template;
-    }
-add_filter ('page_template', 'vendors_redirect_page_template');
